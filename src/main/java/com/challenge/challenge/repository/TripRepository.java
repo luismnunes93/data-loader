12e4c33d6d9da8bc7f3532e2cdc36fe6c17b5e4d
lnunes <luis.m.nunes93@gmail.com>
2023-04-30 14:51:14 +0100

    clean up Query
    "do" was as reserved word in postgresql. (worked in h2, but not on postgresql)

--- src/main/java/com/challenge/challenge/repository/TripRepository.java
+++ src/main/java/com/challenge/challenge/repository/TripRepository.java
@@ -8,38 +8,41 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long>, QuerydslPredicateExecutor<Trip> {
     @Query(value = """
             SELECT zone, pu_total, do_total
-            FROM\s
-                (SELECT pick_up_id, count(id) as pu_total\s
-            \tFROM Trip
-            \tGROUP BY pick_up_id) pu
-            INNER JOIN
-                (SELECT drop_off_id, count(id) as do_total,\s
-            \tFROM Trip
-            \tGROUP BY drop_off_id) do
-            ON (pu.pick_up_id = do.drop_off_id)
+            FROM (
+                SELECT pick_up_id, count(id) as pu_total
+                FROM Trip
+                GROUP BY pick_up_id
+            ) AS pu
+            INNER JOIN (
+                SELECT drop_off_id, count(id) as do_total
+                FROM Trip
+                GROUP BY drop_off_id
+            ) AS doff
+            ON (pu.pick_up_id = doff.drop_off_id)
             INNER JOIN Zone z
-            ON (pu.pick_up_id = z.id)
+            ON pu.pick_up_id = z.id
             ORDER BY pu_total DESC LIMIT :limit""",
             nativeQuery = true)
     List<TopZoneTuple> getTopPickUpByLimit(@Param("limit") Integer limit);
 
     @Query(value = """
             SELECT zone, pu_total, do_total
-            FROM\s
-                (SELECT pick_up_id, count(id) as pu_total\s
-            \tFROM Trip
-            \tGROUP BY pick_up_id) pu
+            FROM
+                (SELECT pick_up_id, count(id) as pu_total
+            FROM Trip
+            GROUP BY pick_up_id) AS pu
             INNER JOIN
-                (SELECT drop_off_id, count(id) as do_total,\s
-            \tFROM Trip
-            \tGROUP BY drop_off_id) do
-            ON (pu.pick_up_id = do.drop_off_id)
+                (SELECT drop_off_id, count(id) as do_total
+            FROM Trip
+            GROUP BY drop_off_id) doff
+            ON (pu.pick_up_id = doff.drop_off_id)
             INNER JOIN Zone z
             ON (pu.pick_up_id = z.id)
             ORDER BY do_total DESC LIMIT ?1""", nativeQuery = true)
@@ -47,23 +50,23 @@ public interface TripRepository extends JpaRepository<Trip, Long>, QuerydslPredi
 
     @Query(value = """
             SELECT zone, pu_total, do_total
-            FROM\s
-                (SELECT pick_up_id, count(pick_up_id) as pu_total\s
+            FROM
+                (SELECT pick_up_id, count(pick_up_id) as pu_total
             FROM Trip
-            WHERE pick_up_date BETWEEN :beginDate AND :endDate\s
-            AND pick_up_id = :zone\s
-            GROUP BY pick_up_id) pu
+            WHERE CAST(pick_up_date as date) >= :beginDate AND CAST(pick_up_date as date) <= :endDate
+            AND pick_up_id = :zone
+            GROUP BY pick_up_id) AS pu
             INNER JOIN
                 (SELECT drop_off_id, count(drop_off_id) as do_total
             FROM Trip
-            WHERE drop_off_date BETWEEN :beginDate AND :endDate\s
-            AND drop_off_id = :zone\s
-            GROUP BY drop_off_id) do
-            ON (pu.pick_up_id = do.drop_off_id)\s
+            WHERE CAST(pick_up_date as date) >= :beginDate AND CAST(pick_up_date as date) <= :endDate
+            AND drop_off_id = :zone
+            GROUP BY drop_off_id) AS doff
+            ON (pu.pick_up_id = doff.drop_off_id)
             INNER JOIN Zone z
             ON (pu.pick_up_id = z.id)
             """, nativeQuery = true)
     List<TopZoneTuple> getSumOfPicksUpsAndDropOffsByZoneAndDate(@Param("zone") Integer zone,
-                                                                @Param("beginDate") String beginDate,
-                                                                @Param("endDate") String endDate);
+                                                                @Param("beginDate") LocalDate beginDate,
+                                                                @Param("endDate") LocalDate endDate);
 }
